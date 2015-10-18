a82d0c22ed0c8ef269b709b72aeed5d31f2aaea0
thatshowifeel <jbeach@traplord.local>
2015-10-17 21:17:09 -0500

    initial commit

---
+++ public/scripts/controllers/Product.js
@@ -0,0 +1,14 @@
+﻿function Product() {
+    var me = this;
+    Product.prototype.listItems = [
+        "Never miss an update from your clients",
+        "Make your dreams come true",
+        "The haters gonna hate hate hate",
+        "I'm just gonna shake it off"
+    ];
+}
+
+function ListItem(text) {
+    var me = this;
+    ListItem.prototype.text = text;
+}
