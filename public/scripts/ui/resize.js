75c74f1084989e7d3233512138d5d693d5adc296
thatshowifeel <jbeach@traplord.local>
2015-10-18 12:10:30 -0500

    Made significant improvemen to mobile

---
+++ public/scripts/ui/resize.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="../../../bower_components/responsive-bootstrap-toolkit/dist/bootstrap-toolkit.js" />
+
+$(function() {
+    setTimeout(function () {
+
+        checkSize(ResponsiveBootstrapToolkit);
+        // Execute code each time window size changes
+        $(window).resize(
+            ResponsiveBootstrapToolkit.changed(function () {
+                checkSize(ResponsiveBootstrapToolkit);
+            })
+        )
+    }, 120);
+});
+
+function checkSize(viewport) {
+    if (viewport.is('xs')) {
+        $("body").css("font-size", "100%");
+    }
+    if (viewport.is('sm')) {
+        $("body").css("font-size", "100%");
+    }
+    if (viewport.is('md')) {
+        $("body").css("font-size", "100%");
+    }
+    if (viewport.is('lg')) {
+        $("body").css("font-size", "125%");
+    }
+}
