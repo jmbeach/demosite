d2317dfd93e74e073dc383d69407a6be457c1513
thatshowifeel <jbeach@traplord.local>
2015-10-19 01:14:26 -0500

    Everything lining up a little better

--- public/scripts/ui/resize.js
+++ public/scripts/ui/resize.js
@@ -2,33 +2,36 @@
 
 $(function () {
     setTimeout(function () {
-
         checkSize(ResponsiveBootstrapToolkit);
         // Execute code each time window size changes
         $(window).resize(
-            ResponsiveBootstrapToolkit.changed(function () {
+            function () {
                 checkSize(ResponsiveBootstrapToolkit);
-            })
+            }
         )
     }, 120);
 });
 
 function checkSize(viewport) {
     if (viewport.is('xs')) {
-        $("body").css("font-size", "100%");
+        $("body").css("font-size", "115%");
         repositionArrow(-18);
+        blurbMargin(true);
     }
     if (viewport.is('sm')) {
-        $("body").css("font-size", "120%");
+        $("body").css("font-size", "135%");
         repositionArrow(68);
+        blurbMargin(false);
     }
     if (viewport.is('md')) {
-        $("body").css("font-size", "110%");
+        $("body").css("font-size", "102%");
         repositionArrow(-35);
+        blurbMargin(false);
     }
     if (viewport.is('lg')) {
         $("body").css("font-size", "125%");
         repositionArrow(-35);
+        blurbMargin(false);
     }
 }
 function repositionArrow(left) {
@@ -37,3 +40,19 @@ function repositionArrow(left) {
         $arrow.css("left", left);
     }
 }
+
+function blurbMargin(isRemove) {
+    var $smBlurb = $("#smallBlurb");
+    var $lgBlurb = $("#bigBlurb");
+    if ($smBlurb.length && $lgBlurb.length) {
+        if (isRemove) {
+            $smBlurb.attr("style", "margin-top:0;");
+            $lgBlurb.attr("style", "margin-top:0;");
+        }
+        else {
+            $smBlurb.attr("style", "");
+            $lgBlurb.attr("style", "");
+        }
+    }
+}
+
